53f5554f411f2c631165a6216a79c2a27ca520e3
Vighna <49743682+VighnaRajan@users.noreply.github.com>
2025-12-17 13:06:36 +0530

    Adding credentials

--- server.js
+++ server.js
@@ -9,22 +9,15 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-const {
-  ADYEN_API_KEY,
-  ADYEN_MERCHANT_ACCOUNT,
-  ADYEN_ENVIRONMENT,
-  PORT
-} = process.env;
+const ADYEN_API_KEY = "AQExhmfxJoPMahdHw0m/n3Q5qf3VZ4VID5pGVFZZwGy8n3ROlODmr0IK7OpPHkNslr/xIBDBXVsNvuR83LVYjEgiTGAH-3EJO2ZjQfvY4N+q8jvOVcZZSzL6enWyMLIsoevlTNGo=-i1iYa2r_Y69~BCYJe@)";
+const ADYEN_MERCHANT_ACCOUNT = "OnebillSoftwareECOM";
+const ADYEN_ENVIRONMENT = "test";
+const PORT = "3000";
 
 const ADYEN_CHECKOUT_URL =
   ADYEN_ENVIRONMENT === "LIVE"
     ? "https://checkout-live.adyen.com/v71"
     : "https://checkout-test.adyen.com/v71";
-
-console.log("process.env",process.env);
-
-console.log("ADYEN_API_KEY",ADYEN_API_KEY);
-console.log("ADYEN_MERCHANT_ACCOUNT",ADYEN_MERCHANT_ACCOUNT);
 
 app.get("/api/paymentMethods", async (req, res) => {
   const response = await fetch(`${ADYEN_CHECKOUT_URL}/paymentMethods`, {
