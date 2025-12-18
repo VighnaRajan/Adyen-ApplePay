716c68e37c16bf6d8b15e4464d9694192bf079c8
Vighna <49743682+VighnaRajan@users.noreply.github.com>
2025-12-18 16:40:00 +0530

    stringify

--- public/applepay.js
+++ public/applepay.js
@@ -24,7 +24,7 @@ document.getElementById('apple-pay-btn').addEventListener('click', async () => {
 
     session.onvalidatemerchant = async (event) => {
       await log('onvalidatemerchant triggered. Requesting merchant session from backend...');
-      await log('event.validationURL', event.validationURL);
+      await log("event", JSON.stringify(event));
       const resp = await fetch('/api/applepay/validate-merchant', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
